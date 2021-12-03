a71d69e5666f7b38c000f01c996d58f23552d785
Jerod Hollen <jerod.a.hollen@gmail.com>
2021-12-03 13:10:48 -0800

    Tweak Video Carousel

--- FinalProject/Flipside/carousel.js
+++ FinalProject/Flipside/carousel.js
@@ -1,12 +1,20 @@
-// Activate Carousel
-$("#myCarousel").carousel();
+let allVids = $("#myCarousel").find('.carousel-item');
 
-// Enable Carousel Indicators
-$(".item").click(function(){
-  $("#myCarousel").carousel(1);
+allVids.each(function(index, el) {
+  if (index !== 0) {
+    $(this).find('video')[0].pause();
+  }
 });
 
-// Enable Carousel Controls
-$(".left").click(function(){
-  $("#myCarousel").carousel("prev");
+$("#myCarousel").on('slide.bs.carousel', function(ev) {
+  let slides = $(this).find('.carousel-item');
+  let pvid = slides[ev.from].querySelectorAll('video')[0];
+  let vid = slides[ev.to].querySelectorAll('video')[0];
+  let isPlaying = vid.currentTime > 0 && vid.readyState > 2;
+
+  vid.play();
+
+  if (isPlaying) {
+    pvid.pause();
+  }
 });
